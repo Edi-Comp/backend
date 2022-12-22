900347712080905a750816fe85a13ae231bd5e12
Zaid Bhimala <zaidm124@gmail.com>
2022-12-22 18:02:45 +0530

    socket io joining room base code added

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
-const app=express();
+const app = express();
+const server = require("http").createServer(app);
+const cors = require("cors");
 
-const PORT=process.env.PORT;
+const path = require("path");
 
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`);
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = process.env.PORT;
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, username) => {
+    console.log("joined room", roomId, "-", username);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", username);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", username);
+    });
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.send("Hey Backend");
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
